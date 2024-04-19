5f529433baa9fa9377d46101b42607f179619452
Javier Cortejoso <javier.cortejoso@gmail.com>
2024-04-19 17:50:27 +0200

    Second commit

--- index.ts
+++ index.ts
@@ -1,27 +1,41 @@
 import { ethers } from "ethers";
 import { GcpKmsSigner } from "ethers-gcp-kms-signer";
 
-const kmsCredentials = {
+const kmsCredentialsBase = {
   projectId: "blockchaintestsglobaltestnet",
   locationId: "global",
-  keyRingId: "jcortejoso-test",
-  keyId: "jcortejoso-test",
+  keyRingId: "cel2-testnet",
   keyVersion: "1",
 };
+let kmsCredentials = [];
+const roles = ["admin", "batcher", "proposer", "sequencer"];
+
+for (const role of roles) {
+  kmsCredentials.push({
+    ...kmsCredentialsBase,
+    keyId: role,
+  });
+}
+
+for (const kmsCredential of kmsCredentials) {
+  console.log(JSON.stringify(kmsCredential, null, 2));
+}
 
 // From ethers, call providers.getDefaultProvider() to get a provider
-const provider = new ethers.providers.JsonRpcProvider("https://baklava-forno.celo-testnet.org");
+const provider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
 
-let signer = new GcpKmsSigner(kmsCredentials);
-signer = signer.connect(provider);
 
 (async () => {
-  const address = await signer.getAddress();
-  console.log("Address:", address); // 0x0e214b0fbf70825cd8d16224f52d570145bc7491
+  for (const kmsCredential of kmsCredentials) {
+    let signer = new GcpKmsSigner(kmsCredential);
+    signer = signer.connect(provider);
+    const address = await signer.getAddress();
+    console.log(`Address for ${kmsCredential.keyId}: ${address}`);
+  }
 
-  const tx = await signer.sendTransaction({
-    to: "0xfCf982bb4015852e706100B14E21f947a5Bb718E",
-    value: ethers.utils.parseEther("0.001"),
-  });
-  console.log("Transaction:", tx);
+  // const tx = await signer.sendTransaction({
+  //   to: "0xfCf982bb4015852e706100B14E21f947a5Bb718E",
+  //   value: ethers.utils.parseEther("0.001"),
+  // });
+  // console.log("Transaction:", tx);
 })();
